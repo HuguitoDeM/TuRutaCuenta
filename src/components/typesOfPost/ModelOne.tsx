730098fcafa128cc1b2e8e986c0003aec4a01f2a
HuguitoDeM <hugojavierdesideriomatinez@gmail.com>
2024-11-08 09:39:23 -0300

    first type of post

---
+++ src/components/typesOfPost/ModelOne.tsx
@@ -0,0 +1,37 @@
+import NavbarDesktop from "../navbar/NavbarDesktop";
+
+const ModelOne = () => {
+  return (
+    <div>
+      <NavbarDesktop login={true} color={true} user="Huguito" />
+      <div>
+        <h2>title</h2>
+      </div>
+      <div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModelOne;
